e7178e49b1ff1323093a9fd57ca31b3288f1372f
Ayebare Tony <ayebaretony@gmail.com>
2025-09-12 14:55:05 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
+// firebase-config.js
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyA9bEKptoXnOyUpZN-e2bZRrt15CWNLVvY",
+    authDomain: "pitchbookingsystem-6e222.firebaseapp.com",
+    projectId: "pitchbookingsystem-6e222",
+    storageBucket: "pitchbookingsystem-6e222.firebasestorage.app",
+    messagingSenderId: "486051720706",
+    appId: "1:486051720706:web:2bbb83988fc6d623c1011b",
+    measurementId: "G-PT7QJYP2T1"
+  };
 
