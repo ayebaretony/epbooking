c8166bad8c2a461355551f57d3ba6cdc16c10a68
Ayebare Tony <ayebaretony@gmail.com>
2025-09-12 15:19:41 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyA9bEKptoXnOyUpZN-e2bZRrt15CWNLVvY",
     authDomain: "pitchbookingsystem-6e222.firebaseapp.com",
     projectId: "pitchbookingsystem-6e222",
@@ -16,8 +7,3 @@
     appId: "1:486051720706:web:2bbb83988fc6d623c1011b",
     measurementId: "G-PT7QJYP2T1"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
